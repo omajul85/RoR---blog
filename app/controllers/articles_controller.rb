362f56c96358c2db94f6252106e8977704e8c999
Omar Alvarez <omajul2@hotmail.com>
2016-08-04 17:01:38 +0100

    Add forms, partials and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,47 @@
 class ArticlesController < ApplicationController
+
+	include ArticlesHelper
+
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
 	def index
 	  @articles = Article.all
 	end
 
 	def show
-		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash.notice = "Article '#{@article.title}' Created!"
+			redirect_to article_path(@article)
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		@article.destroy
+		flash.notice = "Article deleted!"
+		redirect_to articles_path
+	end
+
+	def edit
+	end
+
+	def update
+		# update automatically saves the changes
+		if @article.update(article_params)
+			flash.notice = "Article '#{@article.title}' Updated!"
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
 	end
 
 end
