8082e18793288b03da71b3f7edac15e86c1e1c7e
Omar Alvarez <omajul2@hotmail.com>
2016-08-05 11:38:25 +0100

    Add paperclip gem and implementation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
+	
+	# paperclip related
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	def tag_list
 		# Another alternative is to define a new Tag#to_s method which overrides the default
