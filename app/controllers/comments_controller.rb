70afa3c8416efca90914757c0a6ffba4b80d2772
Omar Alvarez <omajul2@hotmail.com>
2016-08-06 13:54:17 +0100

    Finish authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,12 @@ class CommentsController < ApplicationController
 	def create
 		  @comment = Comment.new(comment_params)
 		  @comment.article_id = params[:article_id]
-		  @comment.save
+		  
+		  unless @comment.save
+		  	flash.notice = "Please enter your name and comment!"
+		  end
 		  redirect_to article_path(@comment.article)
+
 	end
+
 end
